907e07758a10e1b1ff9a27b59642de728123f9a2
Asoul <asoul@Asoul.local>
2015-08-11 23:16:25 +0800

    First Version

---
+++ js/myApp.js
@@ -0,0 +1,33 @@
+var app = angular.module('myApp', ['ui.bootstrap', 'ngAnimate'])
+
+app.controller('BookCarouselCtrl', function ($scope, $http) {
+  
+  $scope.myInterval = 5000;
+  $scope.books = []
+
+  $scope.load = function() {
+    $http.get('https://spreadsheets.google.com/feeds/list/1FjiemWDOuEpE00c0mA1L_WMgB4_WoDfIAp9GHVylOQE/od6/public/values?alt=json')
+    .success(function (response) {
+      books = response.feed.entry
+      numOfBook = books.length
+
+      for (var i = numOfBook - 1; i >= 0; i--) {
+        bookID = books[i].gsx$博客來編號.$t
+        $scope.books.push({
+          title: books[i].gsx$書名.$t,
+          originPrice: books[i].gsx$原價.$t,
+          price: books[i].gsx$售價.$t,
+          discount: books[i].gsx$折扣.$t,
+          image: 'http://www.books.com.tw/img/' + bookID.substring(0, 3) + '/' + bookID.substring(3, 6) + '/' + bookID.substring(6, 8) + '/' + bookID + '.jpg',
+          author: books[i].gsx$作者.$t,
+          isbn: books[i].gsx$isbn.$t,
+          comment: books[i].gsx$註解.$t,
+          buyDate: books[i].gsx$購入日期.$t,
+          buySource: books[i].gsx$購入管道.$t,
+          link: 'http://www.books.com.tw/products/' + bookID
+        })
+      }
+    })
+  }
+
+})
